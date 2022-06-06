2962084ade962723ce46626c7b0af95a59b9db44
akr4log <AKR4log@gmail.com>
2022-06-06 07:44:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 // app.use('/api', router)
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'WORKasdsadasdd!!!' })
